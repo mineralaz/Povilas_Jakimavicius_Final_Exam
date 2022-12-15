fd8e1469178b0636ec939efce2d5350efeb75419
mineralaz <jakimaviciuspovilas@gmail.com>
2022-12-15 14:08:44 +0200

    nav depends on userLoggedIn

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,61 @@
 import { Link, NavLink } from 'react-router-dom';
+import { useAuthCtx } from '../../store/AuthContext';
 import css from './Header.module.css';
 
 function Header(props) {
+  const { userLoggedIn } = useAuthCtx();
   return (
     <header className={css.header}>
       <Link to="/">Logo</Link>
       <nav>
-        <NavLink
-          className={css.navLink}
-          activeClassName={css.activeNavLink}
-          to="/register"
-        >
-          Register
-        </NavLink>
-        <NavLink
-          className={css.navLink}
-          activeClassName={css.activeNavLink}
-          to="/login"
-        >
-          Login
-        </NavLink>
-        <NavLink
-          className={css.navLink}
-          activeClassName={css.activeNavLink}
-          to="/shops"
-        >
-          Shops
-        </NavLink>
-        <NavLink
-          className={css.navLink}
-          activeClassName={css.activeNavLink}
-          to="/add-shop"
-        >
-          Add shop
-        </NavLink>
+        <ul>
+          {!userLoggedIn && (
+            <li>
+              <NavLink
+                className={css.navLink}
+                activeClassName={css.activeNavLink}
+                to="/register"
+              >
+                Register
+              </NavLink>
+            </li>
+          )}
+          {!userLoggedIn && (
+            <li>
+              <NavLink
+                className={css.navLink}
+                activeClassName={css.activeNavLink}
+                to="/login"
+              >
+                Login
+              </NavLink>
+            </li>
+          )}
+
+          {userLoggedIn && (
+            <li>
+              <NavLink
+                className={css.navLink}
+                activeClassName={css.activeNavLink}
+                to="/shops"
+              >
+                Shops
+              </NavLink>
+            </li>
+          )}
+
+          {userLoggedIn && (
+            <li>
+              <NavLink
+                className={css.navLink}
+                activeClassName={css.activeNavLink}
+                to="/add-shop"
+              >
+                Add shop
+              </NavLink>
+            </li>
+          )}
+        </ul>
       </nav>
     </header>
   );
