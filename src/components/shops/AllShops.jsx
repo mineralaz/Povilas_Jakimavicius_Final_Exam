15dce7e166f4849ae9ea98de2c96d93b810952e7
mineralaz <jakimaviciuspovilas@gmail.com>
2022-12-16 10:36:30 +0200

    AllShops component to ShopsPage

---
+++ src/components/shops/AllShops.jsx
@@ -0,0 +1,17 @@
+import { shopsObjectsObjToArr } from '../helpers';
+import useFetch from '../hooks/UseFetch';
+import SingleShop from '../shops/SingleShop';
+
+function AllShops(props) {
+  const url = `${import.meta.env.VITE_REAL_DB_URL}/shops.json`;
+  const [shopsData, setShopsData] = useFetch(url, {});
+  const shopsArr = shopsObjectsObjToArr(shopsData);
+  return (
+    <ul>
+      {shopsArr.map((shopObj) => (
+        <SingleShop key={shopObj.id} obj={shopObj} />
+      ))}
+    </ul>
+  );
+}
+export default AllShops;
--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -1,19 +1,10 @@
-import { shopsObjectsObjToArr } from '../components/helpers';
-import useFetch from '../components/hooks/UseFetch';
-import SingleShop from '../components/shops/SingleShop';
+import AllShops from '../components/shops/AllShops';
 
 function ShopsPage(props) {
-  const url = `${import.meta.env.VITE_REAL_DB_URL}/shops.json`;
-  const [shopsData, setShopsData] = useFetch(url, {});
-  const shopsArr = shopsObjectsObjToArr(shopsData);
-
   return (
     <div className="container">
       <h2>ShopsPage</h2>
-
-      {shopsArr.map((shopObj) => (
-        <SingleShop key={shopObj.id} obj={shopObj} />
-      ))}
+      <AllShops />
     </div>
   );
 }
