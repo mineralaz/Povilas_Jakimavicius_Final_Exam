a5c90829a5f0bccd515e29c8e2fe44926909b5d7
mineralaz <jakimaviciuspovilas@gmail.com>
2022-12-16 09:47:59 +0200

    All shops generated

--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -1,7 +1,19 @@
+import { shopsObjectsObjToArr } from '../components/helpers';
+import useFetch from '../components/hooks/UseFetch';
+import SingleShop from '../components/shops/SingleShop';
+
 function ShopsPage(props) {
+  const url = `${import.meta.env.VITE_REAL_DB_URL}/shops.json`;
+  const [shopsData, setShopsData] = useFetch(url, {});
+  const shopsArr = shopsObjectsObjToArr(shopsData);
+
   return (
     <div className="container">
       <h2>ShopsPage</h2>
+
+      {shopsArr.map((shopObj) => (
+        <SingleShop key={shopObj.id} obj={shopObj} />
+      ))}
     </div>
   );
 }
