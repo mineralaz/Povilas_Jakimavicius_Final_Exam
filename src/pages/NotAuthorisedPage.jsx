892ecc78d3ce4a86da27493354d9359126f1b19b
Povilas <jakimaviciuspovilas@gmail.com>
2023-01-09 17:35:52 +0200

    NotAuthorisedPage loginform added, css

--- src/pages/NotAuthorisedPage.jsx
+++ src/pages/NotAuthorisedPage.jsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
+import LoginAuthForm from '../components/authentification/LoginAuthForm';
 import PageTitle from '../components/UI/PageTitle';
+import css from './NotAuthorisedPage.module.css';
 
 function NotAuthorisedPage(props) {
   return (
     <div className="container">
       <PageTitle>For registered or logged in users only</PageTitle>
-      <p>
-        Register <Link to="/register">Here</Link>
-      </p>
-      <p>
-        Login <Link to="/login">Here</Link>
+      <LoginAuthForm />
+      <p className={css.regText}>
+        Don't have an account?{' '}
+        <Link className={css.regText_link} to="/register">
+          Register Here
+        </Link>
       </p>
     </div>
   );
