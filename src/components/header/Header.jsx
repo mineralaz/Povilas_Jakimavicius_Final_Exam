172bf32983dd345430596e467bf43243eb236f31
Povilas <jakimaviciuspovilas@gmail.com>
2023-01-09 18:14:46 +0200

    header tranparency depends on scroll

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useAuthCtx } from '../../store/AuthContext';
 import Button from '../UI/Button';
@@ -5,8 +6,19 @@ import css from './Header.module.css';
 
 function Header(props) {
   const { userLoggedIn, logout } = useAuthCtx();
+  const [small, setSmall] = useState(false);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', () => {
+        setSmall(window.pageYOffset > 30);
+      });
+    }
+  }, []);
+
   return (
-    <header className={css.header}>
+    <header
+      className={small ? `${css.header} ${css.bgTransparent}` : css.header}
+    >
       <Link to="/" className={css.logo}>
         OurShops
       </Link>
